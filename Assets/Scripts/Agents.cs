bfb9f2c674205401a799a7f042100a92baab28c5
lordfarron <brandon.brunno@gmail.com>
2020-11-24 23:58:15 -0300

    Add Impostor tag

--- Assets/Scripts/Agents.cs
+++ Assets/Scripts/Agents.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Agents : MonoBehaviour{
+public class Agents : MonoBehaviour
+{
 
     public GameObject[] crewList;
     public GameObject crewArray;
@@ -15,23 +16,30 @@ public class Agents : MonoBehaviour{
 
     Vector3[] spawnArea;
 
-    
-    void Start(){
-        Vector3[] spawnArea = new [] {new Vector3(-12.5f, 1.52f, 5.7f), new Vector3(-10.23f, 1.52f, 7.52f), new Vector3(-7.26f, 1.52f, 7.52f),
+
+    void Start()
+    {
+        Vector3[] spawnArea = new[] {new Vector3(-12.5f, 1.52f, 5.7f), new Vector3(-10.23f, 1.52f, 7.52f), new Vector3(-7.26f, 1.52f, 7.52f),
                                       new Vector3(-4.48f, 1.52f,5.7f), new Vector3(-12.5f, 1.52f, 3.46f), new Vector3(-10.23f, 1.52f, 1.83f),
                                       new Vector3(-7.26f, 1.52f, 1.83f), new Vector3(-4.48f, 1.52f, 3.46f)};
-        
+
         _impostorChooser = Random.Range(0, 8);
-        for (int i = 0; i < prefabs.Length; i++){
-            GameObject crewMate =  Instantiate(prefabs[i], spawnArea[i], Quaternion.identity);
+        for (int i = 0; i < prefabs.Length; i++)
+        {
+            GameObject crewMate = Instantiate(prefabs[i], spawnArea[i], Quaternion.identity);
             crewMate.transform.parent = crewArray.transform;
-           
-            crewList = GameObject.FindGameObjectsWithTag("Crewmate");
+
+            crewList = GameObject.FindGameObjectsWithTag("Agent");
             CrewMate crewMateScript = crewMate.gameObject.GetComponent<CrewMate>();
-            foreach (GameObject go in crewList){
-                if(i == _impostorChooser){
+            foreach (GameObject go in crewList)
+            {
+                if (i == _impostorChooser)
+                {
                     crewMateScript.isImpostor = true;
+                    crewMateScript.tag = "Impostor";
                 }
+                else crewMateScript.tag = "Crewmate";
+                
             }
 
 
@@ -44,10 +52,12 @@ public class Agents : MonoBehaviour{
 
     }
 
-    
+
 
     // Update is called once per frame
-    void Update(){
-        
+    void Update()
+    {
+
     }
+
 }
